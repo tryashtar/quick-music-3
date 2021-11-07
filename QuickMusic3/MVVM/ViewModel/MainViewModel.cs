f27692ae6f3a21b889004a7378e2853b987d2163
tryashtar <tryashtar@gmail.com>
2021-11-06 21:08:46 -0600

    Add project files.

---
+++ QuickMusic3/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickMusic3.MVVM.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<PlaylistEntry> Playlist { get; } = new();
+        public MainViewModel()
+        {
+            for (int i = 0; i < 30000; i++)
+            {
+                Playlist.Add(new PlaylistEntry() { Title = i.ToString(), Artist = "art", Album = "whatever", Art = @"D:\Documents\Random Scripts\icon generator\templates\Toby Fox\pixel_Deltarune.png" });
+
+            }
+            Playlist.Add(new PlaylistEntry() { Title = "test", Artist = "art", Album = "whatever" });
+        }
+    }
+
+    public class PlaylistEntry
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Art { get; set; }
+    }
+}
---
+++ QuickMusic3/Themes/TitleBar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuickMusic3
+{
+    public partial class TitleBar : ResourceDictionary
+    {
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (window.WindowState == WindowState.Normal)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = WindowState.Minimized;
+        }
+    }
+}
